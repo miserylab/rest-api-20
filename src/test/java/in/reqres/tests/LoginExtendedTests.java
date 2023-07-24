a4ba6ad8cf4399b7104dd791edc265c83ded504a
Olga Demina <misertylab.r6s@gmail.com>
2023-07-24 19:47:20 +0300

    added Pojo and Lombok models

---
+++ src/test/java/in/reqres/tests/LoginExtendedTests.java
@@ -0,0 +1,81 @@
+package in.reqres.tests;
+
+import in.reqres.models.LoginBodyLombokModel;
+import in.reqres.models.LoginBodyPojoModel;
+import in.reqres.models.LoginResponseLombokModel;
+import in.reqres.models.LoginResponsePojoModel;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginExtendedTests {
+
+    @Test
+    void successfulLoginBadPracticeTest() {
+        String authData = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }"; // BAD PRACTICE
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void successfulLoginWithPojoModelsTest() {
+        LoginBodyPojoModel authData = new LoginBodyPojoModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponsePojoModel loginResponse = given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponsePojoModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
+    }
+
+    @Test
+    void successfulLoginWithLombokModelsTest() {
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModel loginResponse = given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
+    }
+
+}
